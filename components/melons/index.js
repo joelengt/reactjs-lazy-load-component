0d949f908acc3f7b3a8235f0d819c2d4527c38be
Joel Gonzales <joelengt@gmail.com>
2017-12-31 21:11:26 -0500

    Base

---
+++ components/melons/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Melon extends React.Component {
+  render() {
+    return (
+      <div className="Melon">
+        <h2>Melon list</h2>
+      </div>
+    )
+  }
+}
+
+export default Melon
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,55 @@
 import React from 'react'
 import Layout from '../components/Layout'
-import FruitList from '../components/fruits'
+import Melons from '../components/melons'
+import * as Cookies from "js-cookie";
 
-const Index = () => {
-  return (
-    <Layout title="Fruits">
-      <FruitList/>
-    </Layout>
-  )
+class Index extends React.Component {
+  constructor(props) {
+    super(props)
+    this.eventClick = this.eventClick.bind(this)
+    this.state = { fulname: null }
+  }
+
+  componentDidMount () {
+    console.log('DOM READED!!', Cookies.get('fullname'))
+
+    if (Cookies.get('fulname') === undefined) {
+      Cookies.set('fulname', 'joel Gonzales :D')
+    }
+
+    this.setState({
+      fulname: Cookies.get('fulname')
+    })
+
+  }
+
+  componentWillUnmount () {
+    console.log('Final data!')
+  }
+
+  eventClick() {
+    console.log('Data??')
+    console.log('DOM READED INSIDE!!', Cookies.get('fullname'))
+    this.setData()
+  }
+
+  setData() {
+    Cookies.set('fulname', 'joelengt')
+    this.setState({
+      fulname: Cookies.get('fulname')
+    })
+  }
+
+  render() {
+    return (
+      <div className="Index">
+        <h2>Fruit List { this.state.fulname }</h2>
+        <Melons/>
+        <input type="text"/>
+        <button onClick={this.eventClick}>Save</button>
+      </div>
+    )
+  }
 }
 
 export default Index
